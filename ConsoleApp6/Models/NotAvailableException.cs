4f487672dadef515e817a2b2a83bb9013e32bda9
Nesrullah Hacizade <nesrullah.hacizade@gmail.com>
2022-03-25 21:34:25 +0400

    created hotel

---
+++ ConsoleApp6/Models/NotAvailableException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ConsoleApp6.Models
+{
+    internal class NotAvailableException : Exception
+    {
+
+        public NotAvailableException() : base("Ommas")
+        {
+
+        }
+
+    }
+}
---
+++ ConsoleApp6/Models/utilities/NotAvailableException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6.Models.utilities
+{
+    internal class NotAvailableException
+    {
+    }
+}
